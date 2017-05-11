8578e3c5d06951ca53850d46a4d8786d32d192c9
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-05-11 14:36:53 +0900

    Initial Commit

---
+++ TabSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TabSample
+//
+//  Created by SuzukiShigeru on 2017/05/11.
+//  Copyright © 2017年 ShigeruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
